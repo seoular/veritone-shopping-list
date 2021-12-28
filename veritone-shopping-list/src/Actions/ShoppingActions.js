eaf5c9033885ff0ecc8d59889d30e745a29f7c20
seoular <34080384+seoular@users.noreply.github.com>
2021-12-28 11:29:06 -0800

    Delete veritone-shopping-list directory
    remove main directory from branch

--- veritone-shopping-list/src/Actions/ShoppingActions.js
+++
@@ -1,6 +0,0 @@
-
-
-export const addItem = (item) => ({type: 'ADD', payload: item});
-export const deleteItem = (itemID) => ({type: 'DELETE', payload: itemID});
-export const editItem = (itemID, newItem) => ({type: 'EDIT', payload: {itemID: itemID, newItem: newItem}});
-export const selectItem = (itemID) => ({type: 'SELECT', payload: itemID});
